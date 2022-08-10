a875aaa2d1625e417a04b5a1d61c1c571e1fabe7
Boss2021-eng <77675650+Boss2021-eng@users.noreply.github.com>
2022-08-10 15:10:08 +0100

    initial upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The Prime numbers leading to this numbers are: ");
+
+for(int i = 2; i <= num; i++)
+{
+    bool isPrime = true;
+
+    for( int j=2 ; j<i; j++)
+    {
+        if( i % j == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    if(isPrime)
+    {
+        Console.Write( i + "-");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
